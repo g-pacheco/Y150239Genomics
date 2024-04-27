511015de61f4216807761569ef729f2a3f084d41
sbbe-oficial <37849281+g-pacheco@users.noreply.github.com>
2024-04-27 18:12:43 +0200

    ngsAdmix expanded.

--- Y150239Genomics--Pipeline/Y150239Genomics--Plots/Y150239Genomics--ngsAdmix/Y150239Genomics--ngsAdmix.R
+++ Y150239Genomics--Pipeline/Y150239Genomics--Plots/Y150239Genomics--ngsAdmix/Y150239Genomics--ngsAdmix.R
@@ -12,7 +12,7 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 
 # Loads required packages ~
-pacman::p_load(tidyverse, scales, optparse, plyr, RColorBrewer, extrafont, gtable, grid, mdthemes, ggtext, glue)
+pacman::p_load(tidyverse, scales, optparse, plyr, RColorBrewer, extrafont, gtable, grid, mdthemes, ggtext, glue, ggh4x)
 
 
 # Creates colour palette ~
@@ -21,11 +21,12 @@ MyColours <- colorRampPalette(brewer.pal(8, "Paired"))(nb.cols)
 
 
 # Loads the data ~
-samples <- read.table("Leiden.popfile", stringsAsFactors = FALSE, sep = "\t")
+samples.auto <- read.table("Y150239Genomics--ngsAdmix.Autosomes.popfile", stringsAsFactors = FALSE, sep = "\t")
+samples.allo <- read.table("Y150239Genomics--ngsAdmix.Allosome.popfile", stringsAsFactors = FALSE, sep = "\t")
 
 
 # Reads the annotation file ~
-ids <- read.table("NLSparrow.labels", stringsAsFactors = FALSE, sep = "\t", header = FALSE)
+ids <- read.table("Y150239Genomics--ngsAdmix.labels", stringsAsFactors = FALSE, sep = "\t", header = FALSE)
 
 
 # Adds column ids names ~
@@ -38,29 +39,31 @@ ids$Population <- ifelse(grepl("FR0", ids$Sample_ID), "Sales",
                   ifelse(grepl("Lesina", ids$Sample_ID), "Lesina",
                   ifelse(grepl("Crotone", ids$Sample_ID), "Crotone",
                   ifelse(grepl("Guglionesi", ids$Sample_ID), "Guglionesi",
-                  ifelse(grepl("PI22NLD0001M", ids$Sample_ID), "Target",
+                  ifelse(grepl("PI22NLD0001M", ids$Sample_ID), "Y150239",
                   ifelse(grepl("PD22NLD0146F", ids$Sample_ID), "Garderen",
                   ifelse(grepl("PD22NLD0147F", ids$Sample_ID), "Garderen",
-                  ifelse(grepl("PDOM2022NLD0", ids$Sample_ID), "Utrecht", "Error")))))))))
+                  ifelse(grepl("PDOM2022NLD0077M", ids$Sample_ID), "Meerkerk",
+                  ifelse(grepl("PDOM2022NLD0", ids$Sample_ID), "Utrecht", "Error"))))))))))
 
 
 # Reorders Population ~
 ids$Population <- factor(ids$Population, ordered = T,
                          levels = c("Utrecht",
                                     "Garderen",
+                                    "Meerkerk",
                                     "Sales",
                                     "Crotone",
                                     "Guglionesi",
                                     "Lesina",
                                     "Chokpak",
-                                    "Target"))
+                                    "Y150239"))
 
 
 # Expands PCA_Annot by adding Species ~
-ids$Species <- ifelse(ids$Population %in% c("Utrecht", "Sales", "Garderen"), "House",
+ids$Species <- ifelse(ids$Population %in% c("Utrecht", "Sales", "Garderen", "Meerkerk"), "House",
                ifelse(ids$Population %in% c("Chokpak", "Lesina"), "Spanish",
                ifelse(ids$Population %in% c("Guglionesi", "Crotone"), "Italian",
-               ifelse(ids$Population %in% c("Target"), "Uncertain", "Error"))))
+               ifelse(ids$Population %in% c("Y150239"), "Y150239", "Error"))))
 
 
 # Reorders Population ~
@@ -68,23 +71,34 @@ ids$Species <- factor(ids$Species, ordered = T,
                       levels = c("House",
                                  "Italian",
                                  "Spanish",
-                                 "Uncertain"))
+                                 "Y150239"))
+
+
+# Recognises chromosomes ~
+ids.auto <- ids
+ids.allo <- ids
+ids.auto$chrtype <- "Autosomes"
+ids.allo$chrtype <- "Allosome"
 
 
 # Creates data frame ~
-fulldf <- data.frame()
+fulldf.auto <- data.frame()
+fulldf.allo <- data.frame()
 
 
-# Sets order of clusters ~ // This needs to equal the number of Ks you have.
-#x <- list(c(1, 2, 3, 4, 5, 6),
-#          c(1, 2, 3, 4, 5),
-#          c(1, 2, 3, 4),
-#          c(1, 2, 3),
-#         c(1, 2))
+x.auto <- list(c(3, 2, 1, 4, 6, 5, 7),
+               c(4, 5, 1, 2, 6, 3),
+               c(4, 2, 5, 3, 1),
+               c(4, 3, 1, 2),
+               c(1, 3, 2),
+               c(1, 2))
 
-x <- list(c(1, 2, 3, 4, 5, 6, 7),
-          c(1, 2, 3),
-          c(1, 2))
+x.allo <- list(c(5, 6, 3, 1, 4, 2, 7),
+               c(6, 2, 5, 4, 3, 1),
+               c(3, 4, 2, 5, 1),
+               c(3, 4, 2, 1),
+               c(3, 1, 2),
+               c(1, 2))
 
 
 # Defines samples' IDs ~
@@ -92,15 +106,35 @@ sampleid = "Sample_ID"
 
 
 # Loops over all Ks while adding labels and reordering clusters ~
-for (j in 1:length(samples[, 1])){
-  data <- read.table(samples[j, 1])[, x[[j]]]
+for (j in 1:length(samples.auto[, 1])){
+  data <- read.table(samples.auto[j, 1])[, x.auto[[j]]]
   for (i in 1:dim(data)[2]) { 
     temp <- data.frame(Ancestry = data[, i])
     temp$K <- as.factor(rep(i, times = length(temp$Ancestry)))
-    temp[sampleid] <- as.factor(ids[sampleid][,1])
+    temp[sampleid] <- as.factor(ids.auto[sampleid][, 1])
     temp$K_Value <- as.factor(rep(paste("K = ", dim(data)[2], sep = ""), times = length(temp$Ancestry)))
-    temp <- merge(ids, temp)
-    fulldf <- rbind(fulldf, temp)}}
+    temp <- merge(ids.auto, temp)
+    fulldf.auto <- rbind(fulldf.auto, temp)}}
+
+
+for (j in 1:length(samples.allo[, 1])){
+  data <- read.table(samples.allo[j, 1])[, x.allo[[j]]]
+  for (i in 1:dim(data)[2]) { 
+    temp <- data.frame(Ancestry = data[, i])
+    temp$K <- as.factor(rep(i, times = length(temp$Ancestry)))
+    temp[sampleid] <- as.factor(ids.allo[sampleid][, 1])
+    temp$K_Value <- as.factor(rep(paste("K = ", dim(data)[2], sep = ""), times = length(temp$Ancestry)))
+    temp <- merge(ids.allo, temp)
+    fulldf.allo <- rbind(fulldf.allo, temp)}}
+
+
+fulldf <- rbind(fulldf.auto, fulldf.allo)
+
+
+# Reorders chrtye ~
+fulldf$chrtype <- factor(fulldf$chrtype, ordered = T,
+                        levels = c("Autosomes",
+                                    "Allosome"))
 
 
 # Defines the target to be plotted ~
@@ -109,31 +143,33 @@ target = "Population"
 
 # Creates the plot ~
 ngsAdmix <-
- ggplot(fulldf, aes(x = Sample_ID, y = Ancestry, fill = K)) +
- geom_bar(stat = "identity", width = .85, alpha = .70) +
- facet_grid(K_Value ~ get(target), space = "free_x", scales = "free_x") +
- scale_fill_manual(values = c("#ee0000", "#1E90FF", "#FFD700", "#88419d", "#9ecae1", "#c6dbef", "#fec44f")) +
- scale_x_discrete(expand = c(0, 0)) + 
- scale_y_continuous(expand = c(0, 0), breaks = NULL) +
- theme(panel.background = element_rect(fill = "#ffffff"),
-       panel.grid.minor.x = element_blank(),
-       panel.grid.major = element_blank(),
-       panel.spacing = unit(.3, "lines"),
-       plot.title = element_blank(),
-       legend.position = "none",
-       axis.title = element_blank(),
-       #axis.text.x.bottom = element_text(colour = "#000000", face = "bold", angle = 90, vjust = .5, hjust = .5),
-       axis.text.x.bottom = element_blank(),
-       axis.text.y = element_blank(),
-       axis.ticks = element_blank(),
-       strip.background = element_rect(colour = "#000000", fill = "#FAFAFA", linewidth = .05),
-       strip.text.x = element_text(colour = "#000000", face = "bold", family = "Optima", size = 16, angle = 90, margin = margin(.5, .1, .5, .1, "cm")),
-       strip.text.y = element_text(colour = "#000000", face = "bold", family = "Optima", size = 16, angle = 90, margin = margin(0, .1, 0, .1, "cm")))
- 
- 
- # Saves the final plot ~
- ggsave(ngsAdmix, file = "NLSparrow.MinInd90.Autosomes_TMP_SNPs_XXX.pdf",
-        device = cairo_pdf, width = 16, height = 8, dpi = 600)
+ggplot(fulldf, aes(x = Sample_ID, y = Ancestry, fill = K)) +
+  geom_bar(stat = "identity", width = .85, alpha = .70) +
+  facet_nested(chrtype + K_Value ~ Population, scales = "free_x", space = "free_x",
+               strip = strip_nested(background_x = list(element_rect(colour = "#000000", fill = "#FAFAFA", linewidth = .05)),
+                                    background_y = list(element_rect(colour = "#000000", fill = "#bdbdbd", linewidth = .05),
+                                                        element_rect(colour = "#000000", fill = "#FAFAFA", linewidth = .05)), by_layer_y = TRUE)) +
+  scale_fill_manual(values = c("#ee0000", "#1E90FF", "#FFD700", "#88419d", "#9ecae1", "#c6dbef", "#fec44f")) +
+  scale_x_discrete(expand = c(0, 0)) + 
+  scale_y_continuous(expand = c(0, 0), breaks = NULL) +
+  theme(panel.background = element_rect(fill = "#ffffff"),
+        panel.grid.minor.x = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.spacing = unit(.3, "lines"),
+        plot.title = element_blank(),
+        legend.position = "none",
+        axis.title = element_blank(),
+        #axis.text.x.bottom = element_text(colour = "#000000", face = "bold", angle = 90, vjust = .5, hjust = .5),
+        axis.text.x.bottom = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks = element_blank(),
+        strip.text.x = element_text(colour = "#000000", face = "bold", family = "Optima", size = 14, angle = 90, margin = margin(.5, .1, .5, .1, "cm")),
+        strip.text.y = element_text(colour = "#000000", face = "bold", family = "Optima", size = 14, angle = 90, margin = margin(0, .1, 0, .1, "cm")))
+
+
+# Saves the final plot ~
+ggsave(ngsAdmix, file = "XXX.pdf",
+       device = cairo_pdf, width = 20, height = 12, dpi = 600)
 
 
 # Adds grob ~
